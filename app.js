16ad1666e5723341ef3059058c7a03e9a74a8711
nehoehne <77899522+nehoehne@users.noreply.github.com>
2023-03-25 19:42:55 -0500

    styling and first try of nav tab ligic

--- app.js
+++ app.js
@@ -1,6 +1,49 @@
-const resume = document.querySelector("#resume")
-const resumeBtn = document.querySelector("#resume-btn")
+let activePage = null; 
 
-resumeBtn.addEventListener("click", () =>
-    resume.style.display = "block"
-)
+const activateNavigation = () => {
+    const numPages = 4;
+    const navButtons = document.querySelectorAll(".nav-button")
+    const pages = document.querySelectorAll("section");
+
+    if(navButtons.length !== pages.length) {
+        throw new Error("Internal error: nav and content mismatched...")
+    }
+
+    else if(navButtons.length > numPages || pages.length > numPages) {
+        throw new Error("Internal error: missing content..."); 
+    }
+
+    for(let i = 0; i < numPages; i++) {
+        navButtons[i].addEventListener("click", () => {
+            let currPage = pages[i];
+            if(!isActive(currPage)) {
+                toggleDisplay(activePage);
+                activePage = currPage; 
+            } else {
+                activePage = null;
+            }
+            toggleDisplay(currPage);
+        })
+    }
+}
+
+const isActive = (page) => {
+    return activePage === page; 
+}
+
+const toggleDisplay = (element) => {
+    if(element === null) return; 
+
+    if(element.style.display === "block") {
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
+}
+
+const main = () => {
+    console.log("called main!")
+    activateNavigation(); 
+}
+
+main(); 
