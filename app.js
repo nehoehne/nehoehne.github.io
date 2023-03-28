da88b1062d3cbaf90db542044ef1ff71cb6982ba
nehoehne <77899522+nehoehne@users.noreply.github.com>
2023-03-27 22:27:48 -0500

    clean up logic

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
-let activePage = null; 
+const red = "#bc4749";
+const lightGrey = "#fcfcfc"; 
 
-const activateNavigation = () => {
+let activeTab = null; 
+
+class Tab {
+    constructor(btn, content) {
+        this.btn = btn 
+        this.content = content
+        this.isActive = false
+    }
+    activate(isActive) {
+        if(isActive) {
+            this.btn.style.color = red; 
+            this.btn.style.backgroundColor = lightGrey; 
+            this.content.style.display = "block"
+        } else {
+            this.btn.style.color = "black"; 
+            this.btn.style.backgroundColor = "white"; 
+            this.content.style.display = "none"
+        }
+        this.isActive = isActive; 
+    }
+
+}
+
+const enableNavigation = () => {
     const numPages = 4;
-    const navButtons = document.querySelectorAll(".nav-button")
+    const navBtns = document.querySelectorAll(".nav-button")
     const pages = document.querySelectorAll("section");
+    const tabs = []
 
-    if(navButtons.length !== pages.length) {
+    if(navBtns.length !== pages.length) {
         throw new Error("Internal error: nav and content mismatched...")
     }
 
-    else if(navButtons.length > numPages || pages.length > numPages) {
+    else if(navBtns.length > numPages || pages.length > numPages) {
         throw new Error("Internal error: missing content..."); 
     }
 
+    //make tab objects 
+    for(let i = 0; i < numPages; i++) 
+        tabs.push(new Tab(navBtns[i], pages[i]));
+    
     for(let i = 0; i < numPages; i++) {
-        navButtons[i].addEventListener("click", () => {
-            let currPage = pages[i];
-            if(!isActive(currPage)) {
-                toggleDisplay(activePage);
-                activePage = currPage; 
+        navBtns[i].addEventListener("click", function() {
+            let currTab = tabs[i];
+            
+            if(activeTab)
+                activeTab.activate(false)
+
+            if(currTab !== activeTab) {
+                currTab.activate(true);
+                activeTab = currTab; 
             } else {
-                activePage = null;
+                activeTab = null;
             }
-            toggleDisplay(currPage);
         })
     }
 }
@@ -31,19 +63,34 @@ const isActive = (page) => {
     return activePage === page; 
 }
 
-const toggleDisplay = (element) => {
-    if(element === null) return; 
+// const setActiveTab = (shouldActivate, tab, page) {
+//     showContent(shouldActivate, page);
+//     setStyles(shouldActivate, tab); 
+// }
 
-    if(element.style.display === "block") {
-        element.style.display = "none";
-    } else {
-        element.style.display = "block";
-    }
-}
+// const toggleDisplay = (element) => {
+//     if(element === null) return; 
+
+//     if(element.style.display === "block") {
+//         element.style.display = "none";
+//     } else {
+//         element.style.display = "block";
+//     }
+// }
+
+// const toggleActiveTab = (tab) => {
+//     if(!tab) throw new Error(`Unexpected null pointer: page = ${tab}`); 
+    
+//     if(element.style.display === "block") {
+//         element.style.display = "none";
+//     } else {
+//         element.style.display = "block";
+//     }
+// }
 
 const main = () => {
     console.log("called main!")
-    activateNavigation(); 
+    enableNavigation(); 
 }
 
 main(); 
